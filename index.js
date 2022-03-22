5f8f8a5d3f48ef470ad61b3e810b3e7af32ddf8f
restolora <reynan.estolora@volenday.com>
2022-03-22 21:30:51 +0800

    testing new settings for heruko deployment

--- index.js
+++ index.js
@@ -38,11 +38,8 @@ mongoose.connect(
 );
 // api
 const port = process.env.PORT || 3001;
-if (process.env.NODE_ENV === 'production') {
-	app.use(express.static('client'));
-	app.get('*', (req, res) => {
-		req.sendFile(path.resolve(__dirname, 'client', 'out/index.html'));
-	});
-}
-
+app.use(express.static(path.join(__dirname, '/client')));
+app.get('*', (req, res) => {
+	req.sendFile(path.resolve(__dirname, '/client/build', 'index.html'));
+});
 app.listen(port, () => console.log('Express server is running at port', port));
