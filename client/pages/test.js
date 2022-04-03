03ebfb037257b431c3732c9f7993426dece17aaa
restolora <reynan.estolora@volenday.com>
2022-04-04 01:05:28 +0800

    testing removing sdk

--- client/pages/test.js
+++ client/pages/test.js
@@ -1,38 +1,38 @@
 import React, { useEffect, useState } from 'react'
-import { database } from 'sdk'
+// import { database } from 'sdk'
 import { message } from 'antd'
 
 const Test = ({ token }) => {
-  const [data, setData] = useState(null)
-  const Create = async () => {
-    const dt = { Description: 'asdasd' }
-    try {
-      const response = await database
-        .create({ entity: 'About', environment: 'development', token })
-        .data(dt)
-        .exec()
-      message.success(response.message)
-    } catch (error) {
-      console.log(error)
-      message.error(error.message)
-    }
-  }
-  const fetch = async () => {
-    try {
-      const response = await database
-        .get({ entity: 'About', environment: 'development', token })
-        .all()
-        .exec()
-      setData(response.data)
-    } catch (error) {
-      message.error(error.message)
-    }
-  }
-  useEffect(() => {
-    fetch()
-  }, [setData])
-
-  return <>{data && data.map(d => <p>{d.Description}</p>)}</>
+  // const [data, setData] = useState(null)
+  // const Create = async () => {
+  //   const dt = { Description: 'asdasd' }
+  //   try {
+  //     const response = await database
+  //       .create({ entity: 'About', environment: 'development', token })
+  //       .data(dt)
+  //       .exec()
+  //     message.success(response.message)
+  //   } catch (error) {
+  //     console.log(error)
+  //     message.error(error.message)
+  //   }
+  // }
+  // const fetch = async () => {
+  //   try {
+  //     const response = await database
+  //       .get({ entity: 'About', environment: 'development', token })
+  //       .all()
+  //       .exec()
+  //     setData(response.data)
+  //   } catch (error) {
+  //     message.error(error.message)
+  //   }
+  // }
+  // useEffect(() => {
+  //   fetch()
+  // }, [setData])
+  // return <>{data && data.map(d => <p>{d.Description}</p>)}</>
+  return <>testing</>
 }
 
 export default Test
